fd5edc5ce6203e0cb38e859ddcfe163eadee4571
tommaso caputi <56504768+tommaso-caputi@users.noreply.github.com>
2023-10-08 18:38:33 +0200

    init pygame

---
+++ main.py
@@ -0,0 +1,17 @@
+import pygame
+
+pygame.init()
+screen = pygame.display.set_mode((1000, 720))
+clock = pygame.time.Clock()
+running = True
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    screen.fill("black")
+    pygame.display.flip()
+    clock.tick(60)
+
+pygame.quit()
