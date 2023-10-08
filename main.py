aacc4d5542515fdba063a7d92f2264692b3cc134
tommaso caputi <56504768+tommaso-caputi@users.noreply.github.com>
2023-10-08 19:02:48 +0200

    init planet class

--- main.py
+++ main.py
@@ -1,17 +1,26 @@
 import pygame
+import planet
 
 pygame.init()
+
+SCREEN_SIZE = (1000, 720)
 screen = pygame.display.set_mode((1000, 720))
 clock = pygame.time.Clock()
 running = True
+
+pianeta = planet.Planet((255, 0, 0), 20, (100, 100))
 
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-
     screen.fill("black")
+
+    pygame.draw.circle(screen, "yellow", (SCREEN_SIZE[0] / 2, SCREEN_SIZE[1] / 2), 40)
+    pygame.draw.circle(screen, pianeta.color, pianeta.pos, pianeta.size)
+
     pygame.display.flip()
     clock.tick(60)
+
 
 pygame.quit()
---
+++ planet.py
@@ -0,0 +1,5 @@
+class Planet():
+    def __init__(self, color, size, pos):
+        self.color = color
+        self.pos = pos
+        self.size = size
